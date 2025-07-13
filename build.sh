42517ef942c035ea8af24b385c27338f779ab5f7
joreg <joreg@vvvv.org>
2025-07-13 11:27:40 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-cd vvvv.org
+cd stage.vvvv.org
 git pull
-hugo --buildFuture --minify --baseURL https://vvvv.org/ --cacheDir=/home/vvvv/vvvv.org/tmp
+hugo --buildFuture --minify --baseURL https://stage.vvvv.org/ --cacheDir=/home/vvvv/stage.vvvv.org/tmp
