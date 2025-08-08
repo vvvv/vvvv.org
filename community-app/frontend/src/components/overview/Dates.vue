0ba590886c72951f5bc084a989e995c99aa683bb
antongit <anton@vvvv.org>
2025-08-08 11:29:18 +0200

    [overview] dates cosmetics. app build

--- community-app/frontend/src/components/overview/Dates.vue
+++ community-app/frontend/src/components/overview/Dates.vue
@@ -32,13 +32,13 @@ onMounted(async ()=>{
             <template v-else>
                 <div v-if="store.dates.length > 0" class="dates">
                     <template v-for="(item, index) in store.dates.slice(0, 3)">
-                        <div class="mt-0 mb-0 row border-bottom mb-2 pb-3 d-flex align-items-start" :class="{ 'text-muted': item.past }">
-                            <div class="col-2 col-md-3 col-lg-2 pl-0 mr-lg-0 pr-lg-0">
-                                 <a :href="item.link">
-                                     <img :src="item.thumb" class="img-fluid p-1" alt="{{ item.title }}"/>
-                                 </a>
+                        <div class="mt-0 mb-0 row border-bottom mb-2 pb-3 d-flex align-items-center" :class="{ 'text-muted': item.past }">
+                            <div class="col-2 col-lg-1 pl-0 mr-lg-0 pr-lg-0">
+                                <a :href="item.link">
+                                    <img :src="item.thumb" class="img-fluid" alt="{{ item.title }}"/>
+                                </a>
                             </div>
-                            <div class="col-10 col-md-9 col-lg-10 info pl-0">
+                            <div class="col-10 col-lg-11 info">
                                 <a :href="item.link" class="title">{{ item.title }}</a>
                                 <p class="date mt-1 mb-0 d-flex">
                                     <NTag v-if="!item.past" size="small" round :bordered="false" class="mr-2 darkTag">upcoming</NTag>
