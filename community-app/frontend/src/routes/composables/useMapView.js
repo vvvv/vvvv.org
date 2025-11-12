c7e3a6bcef632286ae159f995478451132187b40
antongit <anton@vvvv.org>
2025-11-12 20:02:57 +0100

    Map: MapView for Businesses

--- community-app/frontend/src/routes/composables/useMapView.js
+++ community-app/frontend/src/routes/composables/useMapView.js
@@ -1,3 +1,30 @@
+const addressFields = [
+  { name: 'location_street', required: true },
+  { name: 'location_postalcode', required: true },
+  { name: 'location_city', required: true },
+  { name: 'location_country', required: true },
+  { name: 'location_additionalInfo', required: false }
+];
+
+function makeAddress(item)
+{
+    const address=[];
+    let notComplete = false;
+
+    addressFields.forEach(field => {
+        const value = item[field.name];
+        if (value && value !== '') {
+            address.push(value);
+        }
+        else if (field.required)
+        {
+            notComplete = true;
+        }
+    });
+
+    return { address, notComplete }
+}
+
 export function useMapView(mapRef, handler)
 {
 
@@ -5,14 +32,25 @@ export function useMapView(mapRef, handler)
     const zoom = 4;
 
     function fillMap(items){
+
+        console.log (items[0]);
+
         const locations = items.filter((e)=> e.location !== null)
             .map((e)=>{
+
+                const { address, notComplete } = makeAddress(e);
+
                 return {
                     coords: e.location.coordinates,
-                    html: ` <div>
-                            <img src="${e.logo}" class="mb-2" style="height:60px"/><br>
-                            <div class="mb-2"><strong>${e.name}</strong></div>
-                            <a href="#" id="profileLink">Show Profile</a>
+                    html: ` <div class="pinPopup">
+                                <img src="${e.logo}" class="mb-2" style="height:60px"/><br>
+                                <div class="mb-2"><strong>${e.name}</strong></div>
+                                <div class="address">
+                                    <p class="title">Address:</p>
+                                    <p>${address.join (', ')}</p>
+                                    ${notComplete ? '<p>⚠️ Address is not complete</p>' : ''}
+                                </div>
+                                <a href="#" id="profileLink">Show Profile</a>
                             </div>`,
                     slug: e.slug
                 }
