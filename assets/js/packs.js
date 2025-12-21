f5caa233f40b47477a49f802aeeaed4132be0af6
antongit <anton@vvvv.org>
2025-12-21 11:49:54 +0100

    WIP on packs.js

---
+++ assets/js/packs.js
@@ -0,0 +1,51 @@
+var categories = new Map();
+var packs = new Map();
+
+window.addEventListener ("load", ()=> {
+
+
+    const input = document.querySelector('#filter');
+    
+    input.addEventListener('input', e=>{
+        const query= e.target.value.trim().toLowerCase();
+        filterItems(query);
+    })
+
+    function filterItems(query)
+    {
+        const result = [...packs.entries()].filter((entry)=>{
+
+            console.log (entry.value);
+
+            // value.toLowerCase().includes(query);
+        });
+
+        console.log (result);
+    }
+
+    const catItems = document.querySelectorAll('[data-pack-category]');
+    
+    catItems.forEach((c)=>{
+        categories.set(c.dataset.packCategory, c);
+    })
+
+    // categories[0].disabled=true;
+    // categories[0].classList.remove('active');
+
+    const packItems = document.querySelectorAll('[data-pack]');
+
+    packItems.forEach((p)=>{
+        const infos = p.querySelectorAll('[data-pack-info]');
+        
+        var data = "";
+        
+        infos.forEach(i=>{
+            data += i.textContent;
+        }) 
+
+        packs.set(p, data);
+    })
+
+    // console.log (categories);
+    //console.log (packs);
+}, true);
