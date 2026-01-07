c98a7a95156321162d155e30a7db9d5aeb89dd02
antongit <anton@vvvv.org>
2026-01-07 16:15:00 +0100

    Packs: update after review:
    - added deprecated section
    - also found in added to any list on search
    - owners are listed after title
    - authors are listed after version
    - subcategory sorting fix
    - community/overview/latest packs links to /packs|
    etc

--- assets/js/packs.js
+++ assets/js/packs.js
@@ -1,21 +1,19 @@
 window.addEventListener ("load", ()=> {
 
     const twoWeeks = 14 * 24 * 60 * 60 * 1000;
-    const updatedPacks = [];
     const processing = document.getElementById('packsProcessing');
     const toc = document.getElementById('toc');
-    const dropdownToc = document.getElementById('dropdownToc');
     const found = document.getElementById('filterCount');
     const input = document.querySelector('#filter');
     const sidebar = document.getElementById('sidebar');
     const sidebarBackdrop = document.getElementById('sidebar-backdrop');
     const menuToggle = document.getElementById('menuToggle');
+    const empty = document.querySelector('[data-empty]');
+    const totalSpan = document.getElementById('totalCount');
 
     let query = input.value;
 
     toc.hidden = true;
-
-    // const data = collectData();
 
     let menu = new Map();
     const totalSet = new Set();
@@ -32,39 +30,70 @@ window.addEventListener ("load", ()=> {
     toc.hidden = false;
 
     /////////////////////////////////////////
+           
 
     function buildMenu()
     {
         const content = document.getElementById('v-pills-content');
         const contentDiv = content.querySelector('[data-content]');
+        const title = content.getElementsByTagName('h1')[0];
 
         //Setup Menu Items
         const menuItems = toc.getElementsByTagName('button');
-        // const dropdownItems = dropdownToc.getElementsByTagName('button');
+        const totalPacksCount = Array.from(contentDiv.getElementsByTagName('article')).filter(e=>!e.dataset.deprecated).length;
 
+        if (totalSpan)
+        {
+            totalSpan.getElementsByTagName('span')[0].textContent = totalPacksCount;
+            totalSpan.hidden = false;
+        }
+        
+        title.textContent += ` (${totalPacksCount})`;
+        
         [...menuItems].forEach(button=>{
             const categoryTitle = button.dataset.categoryMenu;
             button.setAttribute("href", "#v-pills-content");
             button.setAttribute("aria-controls", "v-pills-content");
 
+            const menuEntry = menu.get(categoryTitle);
+            
+            // Set menu Inactive if there is no elements
+            let totalCount = menuEntry?.elements?.length;
+            
+            menuEntry?.children.forEach(c=>{
+                totalCount += c.elements.length;
+            });
+
+            if (!totalCount)
+            {
+                const countSpan = menuEntry?.menuItem.querySelector('[data-count]');
+                menuEntry?.menuItem.classList.add('inactive');
+
+                if (countSpan)
+                {
+                    countSpan.hidden = true;
+                }
+            }
+        
+            //On click, push HTML items into the right panel.
             //jquery - replace it with native addeventlistener, when switching to bootstrap 5.
             $(button).on('shown.bs.tab', () => {            
-                const title = content.getElementsByTagName('h1')[0];
+                
                 contentDiv.replaceChildren();
                 
-                showContentForEmpty(false);
+                showContentForEmpty("");
                 
-                title.textContent = categoryTitle;
-                const value = menu.get(categoryTitle);
-                value.elements.forEach(e=>
+                title.textContent = `${categoryTitle} (${totalCount})`;
+
+                menuEntry.elements.forEach(e=>
                 {
                     e.hidden = !isVisible(e);
                     contentDiv.appendChild(e.cloneNode(true));
                 });
 
-                if (value.children.size)
+                if (menuEntry.children.size)
                 {
-                    const sortedSections = Array.from(value.children.values()).sort((a, b)=>{a.name.localeCompare(b.name)});
+                    const sortedSections = Array.from(menuEntry.children.values()).sort((a, b)=>a.name.localeCompare(b.name));
     
                     sortedSections.forEach(c=>{
                         
@@ -107,18 +136,34 @@ window.addEventListener ("load", ()=> {
         
         const menuItems = Array.from(document.querySelectorAll('[data-category-menu]'));
 
-        menu.set("All", {
+        const deprecated = {
+            name: "Deprecated",
+            elements: [],
+            children: new Map(),
+            menuItem: menuItems.find(m=>m.dataset.categoryMenu == "Deprecated")
+        };
+
+        const all = {
             name: "All",
-            elements: elements,
+            elements: elements.filter(e=>!e.dataset.deprecated),
             children: [],
             menuItem: menuItems.find(m=>m.dataset.categoryMenu == "All")
-        })
+        };
 
-        fillUpdated(elements, menuItems);
+        menu.set("All", all)
+        menu.set("Deprecated", deprecated)
+
+        fillUpdated (elements, menuItems, 15);
 
         for (const element of elements)
         {
             let paths;
+            
+            if (element.dataset.deprecated)
+            {
+                deprecated.elements.push(element);
+                continue;
+            }
 
             try {
                 paths = JSON.parse(element.dataset.categories);
@@ -217,23 +262,15 @@ window.addEventListener ("load", ()=> {
         });
     }
 
-    function fillUpdated(elements, menuItems)
+    function fillUpdated(elements, menuItems, count)
     {
-        const updatedPacks = [];
-        const title = "New & Updated";
+        const title = "Latest";
 
-        elements.forEach (e=>{
-            const updated = setNewUpdatedBadge(e); 
-            if (updated) updatedPacks.push(e);
-        });
-
-        updatedPacks.sort((a,b)=>a.element.dataset.pack.localeCompare(b.element.dataset.pack));
+        const updatedPacks = elements.sort((a,b)=>parseInt(a.dataset.lastPublished) < parseInt(b.dataset.lastPublished)).slice(0, count);
+    
+        updatedPacks.sort((a,b)=>parseInt(a.dataset.lastPublished) < parseInt(b.dataset.lastPublished));
         
         const menuItem = menuItems.find(m=>m.dataset.categoryMenu == title);
-        
-        const badge = menuItem.querySelector('[data-badge]');
-        badge.hidden = !updatedPacks.length;
-        badge.textContent = updatedPacks.length;
 
         menu.set(title, {
             name: title,
@@ -349,7 +386,7 @@ window.addEventListener ("load", ()=> {
             }
         })
 
-        showContentForEmpty(totalCount == 0);
+        showContentForEmpty(query);
         
         showFoundCounter();
     }
@@ -408,7 +445,7 @@ window.addEventListener ("load", ()=> {
         if (query == "")
         {
             resetFilter(content, toc);
-            showContentForEmpty(false);
+            showContentForEmpty(query);
             return;
         }
 
@@ -416,17 +453,18 @@ window.addEventListener ("load", ()=> {
         filterContent(content, query);
     }
 
-    function showContentForEmpty(visible)
+    function showContentForEmpty(query = "")
     {
-        const empty = document.querySelector('[data-empty]');
-        empty.replaceChildren();
-        
-        if (!visible)
+
+        if (query == "")
         {
             empty.hidden = true;
             return;
         }
-            
+
+        empty.hidden = false;
+        empty.replaceChildren();
+                    
         const p = document.createElement("p");
         empty.appendChild(p);
                 
@@ -438,8 +476,10 @@ window.addEventListener ("load", ()=> {
         {
             const ul = document.createElement("ul");
             empty.appendChild(ul);
+
+            const packs = count > 1 ? "Packs" : "Pack"; 
             
-            p.textContent = `${count} Packs found in these categories:`;
+            p.textContent = `${count} ${packs} found in these categories:`;
 
             withoutAll.forEach(i=>{
                 const li = document.createElement("li");
@@ -488,7 +528,10 @@ window.addEventListener ("load", ()=> {
         })
 
         document.addEventListener('keydown', (e) => {
-            if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === 'k') {
+
+            const keyLow = e.key.toLowerCase();
+
+            if ((e.ctrlKey || e.metaKey) && (keyLow === 'k' || keyLow === 'f')) {
                 e.preventDefault(); // prevent browser search shortcut
                 input.value = '';
                 input.focus();
@@ -496,6 +539,12 @@ window.addEventListener ("load", ()=> {
             }
 
             if (e.key === 'Escape') {
+                input.value = '';
+                input.focus();
+                input.dispatchEvent(new Event('input', { bubbles: true }));
+            }
+
+            if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === 'f') {
                 input.value = '';
                 input.focus();
                 input.dispatchEvent(new Event('input', { bubbles: true }));
