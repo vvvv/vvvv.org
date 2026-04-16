d2cdf26e0d8910b90d131a14a9e7187583162f21
antongit <anton@vvvv.org>
2026-04-16 14:47:59 +0200

    Hero update

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -2,10 +2,9 @@
 if (window.location.pathname === '/')
 {
     document.addEventListener("DOMContentLoaded", async () => {        
-        const imageURL = (id) => `https://data.vvvv.org/assets/${id}`;
     
         const heroContent = document.getElementById('hero-content');
-        const imageTag = document.getElementById('hero-image');
+        const imageTag = document.getElementById('hero-image').getElementsByTagName('img')[0];
 
         const title = heroContent.getElementsByClassName('title')[0];
         const author = heroContent.getElementsByClassName('author')[0];
@@ -18,8 +17,8 @@ if (window.location.pathname === '/')
         {
             const image = images[index];
 
-            imageTag.setAttribute('src', imageURL(image.image_id));
-            imageTag.setAttribute('srcset', `${imageURL(image.image_id)}, 3072w`);
+            imageTag.setAttribute('src', image.url);
+            imageTag.setAttribute('srcset', `${image.url}, 3072w`);
 
             title.textContent = image.title;
             author.textContent = image.author;
@@ -29,7 +28,7 @@ if (window.location.pathname === '/')
         function makeTagVisible()
         {
             heroContent.style.visibility = 'visible';
-            heroContent.style.opacity = '100';
+            heroContent.style.opacity = '1';
         }
     
         imageTag.addEventListener('load', makeTagVisible);
@@ -40,15 +39,15 @@ if (window.location.pathname === '/')
         });
 
 
-        try{
-            images = await fetchHero();
-            currentIndex=Math.floor(Math.random()*(images.length));
-            setHeroImage(currentIndex);
-        }
-        catch (error) {
-            makeTagVisible();
-            console.log ("Can't fetch project images");
-        }
+        // try{
+        //     images = await fetchHero();
+        //     currentIndex=Math.floor(Math.random()*(images.length));
+        //     setHeroImage(currentIndex);
+        // }
+        // catch (error) {
+        //     makeTagVisible();
+        //     console.log ("Can't fetch project images");
+        // }
 
         async function fetchHero()
         {
@@ -64,12 +63,70 @@ if (window.location.pathname === '/')
 
             return json.data.map (d=>{
                 return {
-                    image_id: d.image,
+                    url: d.image,
                     title: d.title,
                     author: d.author,
                     photographer: d.photographer,
                 }
             })
+        }
+
+        const heroBox = document.getElementById('hero-box');
+
+        const featureTexts = Array.from(heroBox.getElementsByTagName('section'));
+        const featureLines = Array.from(heroBox.getElementsByTagName('li'));
+        const arrows = Array.from(heroBox.getElementsByClassName('arrows')[0].getElementsByTagName('div'));
+
+        let currentFeature = 0;
+
+        if (arrows.length)
+        {
+            const count = featureLines.length;
+            arrows.forEach(arrow => {
+                arrow.addEventListener('click', ()=>{
+                    const nextIndex = arrow.classList.contains('right') ? (currentFeature+1) % count : currentFeature-1 < 0 ? count-1 : currentFeature-1;
+                    featureLines[nextIndex].click();
+                })
+            })
+        }
+
+        if (featureTexts.length)
+        {
+            featureTexts.forEach(text => {
+                
+                const img = text.dataset.img;
+                if (img) 
+                    images.push({
+                        url: img,
+                        title: "",
+                        author: "",
+                        photographer: "",
+                    });
+
+                text.addEventListener('transitionend', ()=>{
+                    if (!text.classList.contains('selected'))
+                    {
+                        text.classList.remove('visible');
+                    }
+                })
+
+            });
+            setHeroImage(0);
+        }
+
+        if (featureLines.length)
+        {
+            featureLines.forEach(line => {
+                line.addEventListener('click', (e) => {
+                    featureLines[currentFeature].classList.remove('selected');
+                    e.target.classList.add('selected');
+                    currentFeature = e.target.dataset.index;
+                    featureTexts.forEach(t => t.classList.remove('selected'));
+                    featureTexts[currentFeature].classList.add('selected', 'visible');
+                    
+                    setHeroImage(currentFeature);
+                });
+            });
         }
     })
 }
--- content/Features/Libraries/index.md
+++ content/Features/Libraries/index.md
@@ -1,4 +1,9 @@
 ---
 title: Libraries
 id: 1
+image: 
+    url: header_70_4_3072.jpg
+    title: Title
+    author: Author
+    photographer: Photographer 
 ---
