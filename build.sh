012113ee064c2872170ea578ba1a20d89272456b
joreg <joreg@vvvv.org>
2026-09-25 21:55:07 +0200

    Update baseURL and cacheDir in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd vvvv.org
 git fetch
 git checkout -f origin/main
-hugo --buildFuture --minify --baseURL https://main.vvvv.org/ --cacheDir=/home/vvvv/main.vvvv.org/tmp
+hugo --minify --baseURL https://vvvv.org/ --cacheDir=/home/vvvv/vvvv.org/tmp
